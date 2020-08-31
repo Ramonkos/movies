6565103d443620f6b28664ac63bef702b7304051
ramon_koestli <ramonkos@gmail.com>
2020-08-31 16:14:35 +0200

    added pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.css';
+
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
-import 'font-awesome/css/font-awesome.css';
+import Pagination from './common/pagination';
+import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
 	state = {
 		movies: getMovies(),
+		pageSize: 4,
+		currentPage: 1,
+	};
+
+	handlePageChange = (page) => {
+		this.setState({ currentPage: page });
 	};
 
 	deleteMovie = (movie) => {
@@ -27,55 +36,63 @@ class Movies extends Component {
 	};
 
 	render() {
+		const { length: count } = this.state.movies;
+		const { currentPage, pageSize, movies: allMovies } = this.state;
+
+		if (count === 0) return <h3>There are no movies in the database</h3>;
+
+		// this will be mapped
+		const movies = paginate(allMovies, currentPage, pageSize);
+
 		return (
 			<>
-				{this.state.movies.length > 0 ? (
-					<>
-						<h3>Showing {this.state.movies.length} movies in the database</h3>
-						<table className='table'>
-							<thead>
-								<tr>
-									<th scope='col'>Title</th>
-									<th scope='col'>Genre</th>
-									<th scope='col'>Stock</th>
-									<th scope='col'>Rate</th>
-									<th></th>
-									<th></th>
+				<h3>Showing {this.state.movies.length} movies in the database</h3>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>Title</th>
+							<th scope='col'>Genre</th>
+							<th scope='col'>Stock</th>
+							<th scope='col'>Rate</th>
+							<th></th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{movies.map((movie) => {
+							return (
+								<tr key={movie._id}>
+									<td>{movie.title}</td>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>{movie.dailyRentalRate}</td>
+									<td>
+										<Like
+											checkLiked={() => this.checkLiked(movie)}
+											onClick={() => this.handleLike(movie)}
+										/>
+									</td>
+									<td>
+										<button
+											onClick={() => {
+												this.deleteMovie(movie);
+											}}
+											className='btn btn-danger'
+										>
+											Delete
+										</button>
+									</td>
 								</tr>
-							</thead>
-							<tbody>
-								{this.state.movies.map((movie) => {
-									return (
-										<tr key={movie._id}>
-											<td>{movie.title}</td>
-											<td>{movie.genre.name}</td>
-											<td>{movie.numberInStock}</td>
-											<td>{movie.dailyRentalRate}</td>
-											<td>
-												<Like
-													checkLiked={() => this.checkLiked(movie)}
-													onClick={() => this.handleLike(movie)}
-												/>
-											</td>
-											<td>
-												<button
-													onClick={() => {
-														this.deleteMovie(movie);
-													}}
-													className='btn btn-danger'
-												>
-													Delete
-												</button>
-											</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</table>
-					</>
-				) : (
-					<h3>There are no movies in the database</h3>
-				)}
+							);
+						})}
+					</tbody>
+				</table>
+				<Pagination
+					itemsCount={count}
+					pageSize={pageSize}
+					currentPage={currentPage}
+					onPageChange={this.handlePageChange}
+				/>
 			</>
 		);
 	}
