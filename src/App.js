0108b5eb33d1576c2f1d438065d8264295b86eea
ramon_koestli <ramonkos@gmail.com>
2020-09-09 16:43:41 +0200

    added main navigation and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import MainNav from './components/MainNav';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
 import Movies from './components/Movies';
+import NotFound from './components/NotFount';
+
+import './App.css';
 
 function App() {
 	return (
-		<main className='container'>
-			<Movies />
-		</main>
+		<>
+			<MainNav />
+			<main className='container'>
+				<Switch>
+					<Route path='/movies' component={Movies} />
+					<Route path='/customers' component={Customers} />
+					<Route path='/rentals' component={Rentals} />
+					<Route path='/not-found' component={NotFound} />
+					<Redirect from='/' exact to='/movies' />
+					<Redirect to='/not-found' />
+				</Switch>
+			</main>
+		</>
 	);
 }
 
--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import Like from './common/Like';
 import Table from './common/Table';
 
