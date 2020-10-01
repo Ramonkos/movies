4a681e094b33073afa593130d89786bfeca3a974
ramon_koestli <ramonkos@gmail.com>
2020-10-01 16:22:52 +0200

    render login and register button conditionally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import jwtDecode from 'jwt-decode';
 
 import MainNav from './components/MainNav';
 import LoginForm from './components/LoginForm';
@@ -14,27 +15,39 @@ import NotFound from './components/NotFount';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-function App() {
-	return (
-		<>
-			<ToastContainer />
-			<MainNav />
-			<main className='container'>
-				<Switch>
-					<Route path='/login' component={LoginForm} />
-					<Route path='/register' component={RegisterForm} />
-					<Route path='/movies/:id' component={MovieForm} />
-					<Route path='/movies/new' component={MovieForm} />
-					<Route path='/movies' component={Movies} />
-					<Route path='/customers' component={Customers} />
-					<Route path='/rentals' component={Rentals} />
-					<Route path='/not-found' component={NotFound} />
-					<Redirect from='/' exact to='/movies' />
-					<Redirect to='/not-found' />
-				</Switch>
-			</main>
-		</>
-	);
+class App extends Component {
+	state = {};
+
+	componentDidMount() {
+		try {
+			const jwt = localStorage.getItem('token');
+			const user = jwtDecode(jwt);
+			this.setState({ user });
+		} catch (error) {}
+	}
+
+	render() {
+		return (
+			<>
+				<ToastContainer />
+				<MainNav user={this.state.user} />
+				<main className='container'>
+					<Switch>
+						<Route path='/login' component={LoginForm} />
+						<Route path='/register' component={RegisterForm} />
+						<Route path='/movies/:id' component={MovieForm} />
+						<Route path='/movies/new' component={MovieForm} />
+						<Route path='/movies' component={Movies} />
+						<Route path='/customers' component={Customers} />
+						<Route path='/rentals' component={Rentals} />
+						<Route path='/not-found' component={NotFound} />
+						<Redirect from='/' exact to='/movies' />
+						<Redirect to='/not-found' />
+					</Switch>
+				</main>
+			</>
+		);
+	}
 }
 
 export default App;
