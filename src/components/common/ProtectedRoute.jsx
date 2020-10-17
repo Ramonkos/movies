d6ca944480b43f5e5743b5bf9592c0ff9a306baf
ramon_koestli <ramonkos@gmail.com>
2020-10-17 13:09:22 +0200

    forward user where he came from after login

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -5,9 +5,18 @@ import auth from '../../services/authService';
 const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
 	return (
 		<Route
-      {...rest}
+			{...rest}
 			render={(props) => {
-				if (!auth.getCurrentUser()) return <Redirect to='/login' />;
+				console.log(props);
+				if (!auth.getCurrentUser())
+					return (
+						<Redirect
+							to={{
+								pathname: '/login',
+								state: { from: props.location },
+							}}
+						/>
+					);
 				return Component ? <Component {...props} /> : render(props);
 			}}
 		/>
